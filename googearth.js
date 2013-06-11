de0de4a0180a1468b4a518942d32b0928587e23d
Ahmed Fasih <wuzzyview@gmail.com>
2013-06-11 19:45:02 -0400

    Google Earth API produces Snyder's figure 37
    Tilted perspective

--- googearth.js
+++ googearth.js
@@ -8,15 +8,40 @@ function init() {
 function initCB(instance) {
     ge = instance;
     ge.getWindow().setVisibility(true);
+    ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
+    ge.getOptions().setGridVisibility(true);
 
     var radius_of_earth = 6371e3; // meters
-    var P = 1.81; // unitless, Snyder's variable name
+
+    // My estimate of Harrison's "Europe from the East"
+    var Peurope = 1.81;
+    var lateurope = 55.9;
+    var loneurope = 58.3;
+    var roleurope = 82.1;
+    var tileurope = -12.0;
+
+    // Snyder's figure 37
+    var Ps = 160e3 / radius_of_earth + 1.0;
+    var lats = 41.0;
+    var lons = -74.0;
+    var tils = 55;
+    var rols = 210;
+
+    var P = Ps;
+    var lat = lats;
+    var lon = lons;
+    var til = tils;
+    var rol = rols;
+
     var height = (P-1.0) * radius_of_earth;
 
     var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
-    camera.setLatitude(55.9);
-    camera.setLongitude(58.3);
+    camera.setLatitude(lat);
+    camera.setLongitude(lon);
     camera.setAltitude(height);
+
+    camera.setHeading(rols);
+    camera.setTilt(til);
 
     ge.getView().setAbstractView(camera);
 }
